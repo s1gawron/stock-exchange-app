54b105ad5f96a48a1a8f2b62b7605066f7559fb2
Sebastian <gawron241@gmail.com>
2019-11-21 10:04:20 +0100

    working

---
+++ src/main/java/Json.java
@@ -0,0 +1,26 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.Objects;
+
+class Json {
+    static JsonObject getJson() {
+        File jsonFile = new File((Objects.requireNonNull(User.class.getClassLoader().getResource("user.json"))).getFile());
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(jsonFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return new Gson().fromJson(Objects.requireNonNull(fileReader), (Type) JsonObject.class);
+    }
+
+    static void editJson(String name, double price, int quantity) {
+        Json.getJson().getAsJsonObject("stock").addProperty("stockName", name);
+        Json.getJson().getAsJsonObject("stock").addProperty("stockPrice", price);
+        Json.getJson().getAsJsonObject("stock").addProperty("stockQuantity", quantity);
+    }
+}
