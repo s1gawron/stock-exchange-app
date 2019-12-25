227e0914972842780a215ff7eb1425325cd88fdf
Sebastian <gawron241@gmail.com>
2019-12-25 15:00:55 +0100

    probably finished

--- src/main/java/StockActions.java
+++ src/main/java/StockActions.java
@@ -1,13 +1,10 @@
-import lombok.ToString;
-
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.stream.Stream;
 
 class StockActions {
 
-    private static boolean containsName(final List<StockWIG20> list, final String name) {
-        return list.stream().anyMatch(o -> o.getName().equals(name));
+    private static boolean containsStock(final List<StockWIG20> list, final String ticker) {
+        return list.stream().anyMatch(o -> o.getTicker().equals(ticker));
     }
 
     private static void openStock() {
@@ -22,6 +19,23 @@ class StockActions {
         } else {
             System.out.println("Gielda otwarta!");
         }
+    }
+
+    private static void removeStockFromList(final List<StockWIG20> list, final String ticker) {
+        list.stream()
+                .filter(o -> o.getTicker().equals(ticker) && o.getQuantity() == 0)
+                .forEach(list::remove);
+    }
+
+    private static void settingParamsOfWallet(User user, int quantity, StockWIG20 stockWIG20, int ratio) {
+        user.setStockValue(user.getStockValue() + (ratio * (quantity * stockWIG20.getTempPrice())));
+        user.setBalanceAvailable(user.getBalanceAvailable() - (ratio * (quantity * stockWIG20.getTempPrice())));
+    }
+
+    private static void settingStockQuantity(final List<StockWIG20> list, String ticker, int quantity, int ratio) {
+        list.stream()
+                .filter(o -> o.getTicker().equals(ticker))
+                .forEach(o -> o.setQuantity(o.getQuantity() + (ratio * quantity)));
     }
 
     private static void timeOfTransaction() {
@@ -41,47 +55,48 @@ class StockActions {
 
         openStock();
 
-        if (user.getBalanceAvailable() >= quantity * StockWIG20.getMap().get(ticker).getTempPrice()) {
-            timeOfTransaction();
+        if (user.getBalanceAvailable() >= quantity * stockWIG20.getTempPrice() && quantity > 0) {
+//            timeOfTransaction();
 
 //            Dodanie akcji do konta uzytkownika, gdy uzytkownik posiada akcje ktore chce kupic:
-            if (containsName(userStock, stockWIG20.getName())) {
-                userStock.stream()
-                        .filter(o -> o.getTicker().equals(ticker))
-                        .forEach(o -> o.setQuantity(o.getQuantity() + quantity));
+            if (containsStock(userStock, ticker)) {
+                settingStockQuantity(userStock, ticker, quantity, 1);
             } else { // Gdy nie posiada akcji:
                 userStock.add(stockWIG20);
                 stockWIG20.setQuantity(quantity);
                 user.setUserStock(userStock);
             }
-
-            user.setStockValue(user.getStockValue() + (quantity * StockWIG20.getMap().get(ticker).getTempPrice()));
-            user.setBalanceAvailable(user.getBalanceAvailable() - (quantity * StockWIG20.getMap().get(ticker).getTempPrice()));
+            settingParamsOfWallet(user, quantity, StockWIG20.getMap().get(ticker), 1);
             User.serializeUser(user);
-
         } else {
             int maxAmount = (int) Math.floor((user.getBalanceAvailable() / (StockWIG20.getMap().get(ticker).getTempPrice())));
-            System.out.println("Nie masz odpowiednich srodkow, mozesz zakupic: " + maxAmount);
+            System.out.println("Nie masz odpowiednich srodkow, maksymalna ilosc akcji jakie mozesz kupic: " + maxAmount);
         }
     }
 
-    static void stockSell(int quanity, String ticker) {
+    static void stockSell(int quantity, String ticker) {
         User user = User.deserializeUser();
         List<StockWIG20> userStock = new CopyOnWriteArrayList<>(user.getUserStock());
-        StockWIG20 stockWIG20 = StockWIG20.getMap().get(ticker);
 
         openStock();
 
-        if (containsName(userStock, stockWIG20.getName())) {
+        if (containsStock(userStock, ticker)) {
+//            timeOfTransaction();
+
             Optional<Integer> amountOfStock = userStock.stream()
                     .filter(o -> o.getTicker().equals(ticker))
                     .map(StockWIG20::getQuantity)
                     .findFirst();
 
-//            if (quanity <= amountOfStock.get()) {
-//
-//            }
-
+            if (quantity <= amountOfStock.get() && quantity > 0) {
+                settingStockQuantity(userStock, ticker, quantity, -1);
+                settingParamsOfWallet(user, quantity, StockWIG20.getMap().get(ticker), -1);
+                removeStockFromList(userStock, ticker);
+                user.setUserStock(userStock);
+                User.serializeUser(user);
+            } else {
+                System.out.println("Nie posiadasz takiej ilosci akcji! Ilosc akcji w Twoim portfelu: " + amountOfStock.get());
+            }
         } else {
             System.out.println("Nie posiadasz tych akcji!");
         }
