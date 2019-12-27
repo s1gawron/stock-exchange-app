51ddb255007ddec7d2ed3c95e203ae9b39a99b62
Sebastian <gawron241@gmail.com>
2019-12-27 10:06:16 +0100

    Main menu

--- src/main/java/MainMenu.java
+++ src/main/java/MainMenu.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.Scanner;
 
@@ -13,17 +14,27 @@ public class MainMenu {
             System.out.println("Podaj ilosc akcji ktore chcesz sprzedac: ");
             quantity = scanner.nextInt();
             StockActions.stockSell(quantity, stockChoice);
-            System.out.println("Transakcja przebiegla pomyslnie.");
         } else if ((action == 1) && (choiceYN.equals("Y") || choiceYN.equals("y"))) {
             System.out.println("Podaj symbol spolki ktora chcesz kupic: ");
             stockChoice = scanner.nextLine();
             System.out.println("Podaj ilosc akcji ktore chcesz kupic: ");
             quantity = scanner.nextInt();
             StockActions.stockPurchase(quantity, stockChoice);
-            System.out.println("Transakcja przebiegla pomyslnie.");
-        } else {
-            return;
         }
+    }
+
+    private static boolean hiddenActions() {
+        Calendar calendar = Calendar.getInstance();
+        boolean showActions;
+
+        if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY || calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+            showActions = false;
+        } else if (calendar.get(Calendar.HOUR_OF_DAY) < 9 || calendar.get(Calendar.HOUR_OF_DAY) > 17) {
+            showActions = false;
+        } else {
+            showActions = true;
+        }
+        return showActions;
     }
 
     static void mainMenu() {
@@ -44,40 +55,33 @@ public class MainMenu {
             choice = scanner.nextInt();
             scanner.nextLine();
 
-            switch (choice) {
-                case 1:
-                    System.out.flush();
-                    System.out.println("\nKonto: " + user.getName());
-                    System.out.println("Dostepne saldo: " + user.getBalanceAvailable());
-                    System.out.println("Wartosc posiadanych akcji przez Ciebie akcji wynosi: " + user.getStockValue());
-                    System.out.println("Wartosc Twojego portfela wynosi: " + user.getWalletValue());
-                    System.out.println("Poprzednia wartosc Twojego portfela wynosila: " + user.getPrevWalletValue());
-                    System.out.println("Procentowa zmiana wartosci portfela: ");
+            if (choice == 1) {
+                System.out.println("\nKonto: " + user.getName());
+                System.out.println("Dostepne saldo: " + user.getBalanceAvailable());
+                System.out.println("Wartosc posiadanych akcji przez Ciebie akcji wynosi: " + user.getStockValue());
+                System.out.println("Wartosc Twojego portfela wynosi: " + user.getWalletValue());
+                System.out.println("Poprzednia wartosc Twojego portfela wynosila: " + user.getPrevWalletValue());
+                System.out.println("Procentowa zmiana wartosci portfela: ");
 
-                    if (user.getUserStock().isEmpty()) {
-                        System.out.println("Nie posiadasz zadnych akcji.");
-                    } else {
-                        System.out.println("Akcje ktore posiadasz: " + user.getUserStock());
-                        while (!StockActions.hideActions()) {
-                            System.out.println("\nCzy chcialbys sprzedac jakies akcje? (Y/N):");
-                            String sellChoice = scanner.nextLine();
-                            getUserDecision(sellChoice, 0);
-                        }
+                if (user.getUserStock().isEmpty()) {
+                    System.out.println("Nie posiadasz zadnych akcji.");
+                } else {
+                    System.out.println("Akcje ktore posiadasz: " + user.getUserStock());
+                    if (hiddenActions()) {
+                        System.out.println("\nCzy chcialbys sprzedac jakies akcje? (Y/N):");
+                        String sellChoice = scanner.nextLine();
+                        getUserDecision(sellChoice, 0);
                     }
-                    break;
-                case 2:
-                    System.out.println(StockWIG20.getMap());
-                    while (!StockActions.hideActions()) {
-                        System.out.println("\nCzy chcialbys kupic jakies akcje? (Y/N):");
-                        String purchaseChoice = scanner.nextLine();
-                        getUserDecision(purchaseChoice, 1);
-                    }
-                    break;
-                case 3:
-                    break;
-                default:
-                    System.out.println("\nNie istnieje taka opcja");
-                    break;
+                }
+            } else if (choice == 2) {
+                System.out.println("\n" + StockWIG20.getMap());
+                if (hiddenActions()) {
+                    System.out.println("\nCzy chcialbys kupic jakies akcje? (Y/N):");
+                    String purchaseChoice = scanner.nextLine();
+                    getUserDecision(purchaseChoice, 1);
+                }
+            } else {
+                System.out.println("\nNie istnieje taka opcja");
             }
         } while (choice != 3);
         System.out.println("\nDo zobaczenia!");
