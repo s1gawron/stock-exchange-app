2f9d90f44c8ad18bd9c35d2649f19c0fe31fb94b
Sebastian <gawron241@gmail.com>
2020-02-05 21:01:38 +0100

    Added UserDao class

--- src/main/java/pl/eizodev/app/UserDao.java
+++ src/main/java/pl/eizodev/app/UserDao.java
@@ -1,4 +1,44 @@
 package pl.eizodev.app;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
 public class UserDao {
+
+    public User getUser(Long id) {
+        Session session = HibernateConfig.INSTANCE.getSessionFactory().openSession();
+        User user = session.get(User.class, id);
+        session.close();
+        return user;
+    }
+
+    public void addUser(User user) {
+        Session session = HibernateConfig.INSTANCE.getSessionFactory().openSession();
+        Transaction transaction = session.getTransaction();
+        try {
+            transaction.begin();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public void updateUser(Long id, User user) {
+        Session session = HibernateConfig.INSTANCE.getSessionFactory().openSession();
+        Transaction transaction = session.getTransaction();
+        try {
+            transaction.begin();
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
 }
