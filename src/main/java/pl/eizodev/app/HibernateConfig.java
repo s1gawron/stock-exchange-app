ca2fa915593a0e28b86e974c0dcc6636dc7780f8
Sebastian <gawron241@gmail.com>
2020-02-05 21:00:24 +0100

    Added hibernate config

---
+++ src/main/java/pl/eizodev/app/HibernateConfig.java
@@ -0,0 +1,19 @@
+package pl.eizodev.app;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public enum HibernateConfig {
+
+    INSTANCE;
+
+    public SessionFactory getSessionFactory() {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        MetadataSources sources = new MetadataSources(registry);
+        Metadata metadata = sources.getMetadataBuilder().build();
+        return metadata.getSessionFactoryBuilder().build();
+    }
+}
