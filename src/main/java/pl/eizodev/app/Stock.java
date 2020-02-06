709d4e8eff339daa2780934d3fcdb0c2a652c0f6
Sebastian <gawron241@gmail.com>
2020-02-06 19:52:02 +0100

    embeddable

--- src/main/java/pl/eizodev/app/Stock.java
+++ src/main/java/pl/eizodev/app/Stock.java
@@ -2,17 +2,28 @@ package pl.eizodev.app;
 
 import lombok.*;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
 @Data
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Embeddable
 public class Stock {
+    @Column(name = "ticker")
     private String ticker;
+    @Column(name = "name")
     private String name;
+    @Column(name = "price")
     private float price;
+    @Column(name = "averagePurchasePrice")
     private float averagePurchasePrice;
+    @Column(name = "change")
     private String change;
+    @Column(name = "volume")
     private String volume;
+    @Column(name = "quantity")
     private int quantity;
 }
