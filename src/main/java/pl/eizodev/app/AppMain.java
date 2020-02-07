9873e338503bc2eb493affdf9f87cdaaea974d15
Sebastian <gawron241@gmail.com>
2020-02-07 16:33:28 +0100

    AppMain

--- src/main/java/pl/eizodev/app/AppMain.java
+++ src/main/java/pl/eizodev/app/AppMain.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class AppMain extends SpringBootServletInitializer {
     public static void main(String[] args) {
@@ -11,8 +15,14 @@ public class AppMain extends SpringBootServletInitializer {
 //        mainMenu.start();
 //        SpringApplication.run(AppMain.class, args);
         UserDao userDao = new UserDao();
-        User user = new User();
-        user = user.deserializeUser();
+        List<Stock> stocks = new ArrayList<>();
+        StockWIG20 stockWig20 = new StockWIG20();
+        Stock stock = stockWig20.getByTicker("CDR");
+        stock.setQuantity(10);
+        stocks.add(stock);
+        User user = new User(1L, "testSebastian", LocalDate.now(), 0, 10000, 10000, 10000, stocks);
+
         userDao.addUser(user);
+        System.out.println(userDao.getUser(1L));
     }
 }
