f78a93e7474d8c1f21c867fe5e17f4f91b5224a0
Sebastian <gawron241@gmail.com>
2020-02-07 21:33:28 +0100

    Hibernate

--- src/main/java/pl/eizodev/app/Stock.java
+++ src/main/java/pl/eizodev/app/Stock.java
@@ -4,26 +4,26 @@ import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import java.io.Serializable;
 
-@Data
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Embeddable
-public class Stock {
-    @Column(name = "ticker")
+public class Stock implements Serializable {
+    @Column(name = "TICKER")
     private String ticker;
-    @Column(name = "name")
+    @Column(name = "NAME")
     private String name;
-    @Column(name = "price")
+    @Column(name = "PRICE")
     private float price;
-    @Column(name = "averagePurchasePrice")
+    @Column(name = "AVERAGE_PRICE")
     private float averagePurchasePrice;
-    @Column(name = "change")
+    @Column(name = "CHANGE")
     private String change;
-    @Column(name = "volume")
+    @Column(name = "VOLUME")
     private String volume;
-    @Column(name = "quantity")
+    @Column(name = "QUANTITY")
     private int quantity;
 }
