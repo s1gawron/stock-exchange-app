d4de9161ca3c42384dc5369ea4737197af749e03
Sebastian <gawron241@gmail.com>
2020-02-11 11:53:32 +0100

    Stock

--- src/main/java/pl/eizodev/app/Stock.java
+++ src/main/java/pl/eizodev/app/Stock.java
@@ -2,28 +2,38 @@ package pl.eizodev.app;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import java.io.Serializable;
+import javax.persistence.*;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Embeddable
-public class Stock implements Serializable {
-    @Column(name = "TICKER")
+@Entity
+@Table(name = "stock")
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long stockId;
+    @Column(name = "ticker")
     private String ticker;
-    @Column(name = "NAME")
+    @Column(name = "name")
     private String name;
-    @Column(name = "PRICE")
+    @Column(name = "price")
     private float price;
-    @Column(name = "AVERAGE_PRICE")
+    @Column(name = "averagePrice")
     private float averagePurchasePrice;
-    @Column(name = "CHANGE")
+    @Column(name = "change")
     private String change;
-    @Column(name = "VOLUME")
+    @Column(name = "volume")
     private String volume;
-    @Column(name = "QUANTITY")
+    @Column(name = "quantity")
     private int quantity;
+    @ManyToOne(cascade = {
+            CascadeType.PERSIST,
+            CascadeType.DETACH,
+            CascadeType.MERGE,
+            CascadeType.REFRESH
+    })
+    @JoinColumn(name = "idUser")
+    private User user;
 }
