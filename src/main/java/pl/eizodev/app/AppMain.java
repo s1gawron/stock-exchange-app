545348092e566560968e3f6848edfdf2aa2a072f
Sebastian <gawron241@gmail.com>
2020-02-14 12:58:45 +0100

    App Main

--- src/main/java/pl/eizodev/app/AppMain.java
+++ src/main/java/pl/eizodev/app/AppMain.java
@@ -1,8 +1,11 @@
 package pl.eizodev.app;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import pl.eizodev.app.dao.StockDao;
+import pl.eizodev.app.dao.UserDao;
+import pl.eizodev.app.entity.Stock;
+import pl.eizodev.app.entity.User;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -15,14 +18,20 @@ public class AppMain extends SpringBootServletInitializer {
 //        mainMenu.start();
 //        SpringApplication.run(AppMain.class, args);
         UserDao userDao = new UserDao();
-        List<Stock> stocks = new ArrayList<>();
+        StockDao stockDao = new StockDao();
         StockWIG20 stockWig20 = new StockWIG20();
         Stock stock = stockWig20.getByTicker("CDR");
         stock.setQuantity(10);
-        stocks.add(stock);
-        User user = new User(1L, "testSebastian", LocalDate.now(), 0, 10000, 10000, 10000, stocks);
+        User user = new User();
+        user.setName("testSebastian2");
+        user.setBalanceAvailable(10000);
+        user.setWalletValue(10000);
+        user.setPrevWalletValue(10000);
+        user.setUserUpdate(LocalDate.now());
+        user.getUserStock().add(stock);
 
         userDao.addUser(user);
-        System.out.println(userDao.getUser(1L).getBalanceAvailable());
+        stockDao.addStock(stock);
+        System.out.println(userDao.getUser(1L));
     }
 }
