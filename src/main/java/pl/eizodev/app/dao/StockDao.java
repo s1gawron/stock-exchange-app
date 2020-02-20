7116ae87e4cf89c9240cc86e72801038fda0f630
Sebastian <gawron241@gmail.com>
2020-02-20 09:33:42 +0100

    StockDao

--- src/main/java/pl/eizodev/app/dao/StockDao.java
+++ src/main/java/pl/eizodev/app/dao/StockDao.java
@@ -3,7 +3,11 @@ package pl.eizodev.app.dao;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import pl.eizodev.app.HibernateConfig;
+import pl.eizodev.app.StockWIG20;
 import pl.eizodev.app.entity.Stock;
+import pl.eizodev.app.entity.User;
+
+import java.util.List;
 
 public class StockDao {
     public void addStock(Stock stock) {
@@ -21,12 +25,19 @@ public class StockDao {
         }
     }
 
-    //TO DO
-    public void updateStock(Long id, Stock stock) {
+    public void updateStock(Long id) {
         Session session = HibernateConfig.INSTANCE.getSessionFactory().openSession();
         Transaction transaction = session.getTransaction();
         try {
             transaction.begin();
+            User user = session.find(User.class, id);
+            List<Stock> userStocks = user.getUserStock();
+            StockWIG20 stock = new StockWIG20();
+
+            userStocks
+                    .forEach(o -> o.setPrice(stock.getByTicker(o.getTicker()).getPrice()));
+
+            transaction.commit();
         } catch (Exception e) {
             transaction.rollback();
             e.printStackTrace();
