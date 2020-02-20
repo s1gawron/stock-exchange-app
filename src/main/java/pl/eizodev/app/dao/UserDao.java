c4cfda5b8c382ebad52a10d2f6d26d1f7e9ff8ac
Sebastian <gawron241@gmail.com>
2020-02-20 09:33:55 +0100

    UserDao

--- src/main/java/pl/eizodev/app/dao/UserDao.java
+++ src/main/java/pl/eizodev/app/dao/UserDao.java
@@ -3,7 +3,11 @@ package pl.eizodev.app.dao;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import pl.eizodev.app.HibernateConfig;
+import pl.eizodev.app.entity.Stock;
 import pl.eizodev.app.entity.User;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class UserDao {
     public User getUser(Long id) {
@@ -34,7 +38,19 @@ public class UserDao {
         try {
             transaction.begin();
             User user = session.find(User.class, id);
+            List<Stock> userStocks = user.getUserStock();
 
+            userStocks
+                    .forEach(o -> user.setStockValue(o.getQuantity() * o.getPrice()));
+
+            user.setWalletValue(user.getStockValue() + user.getBalanceAvailable());
+
+            if (LocalDate.now().isAfter(user.getUserUpdate())) {
+                user.setPrevWalletValue(user.getWalletValue());
+            }
+
+            user.setUserUpdate(LocalDate.now());
+            transaction.commit();
         } catch (Exception e) {
             transaction.rollback();
             e.printStackTrace();
