87ba6a616f23ead57fa8798e5af8d79a2bbef0ff
Sebastian <gawron241@gmail.com>
2020-03-07 13:31:32 +0100

    StockActions

--- src/main/java/pl/eizodev/app/StockActions.java
+++ src/main/java/pl/eizodev/app/StockActions.java
@@ -1,6 +1,8 @@
 package pl.eizodev.app;
 
-import pl.eizodev.app.dao.UserDao;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import pl.eizodev.app.dao.StockDao;
 import pl.eizodev.app.entity.Stock;
 import pl.eizodev.app.entity.User;
 
@@ -8,95 +10,95 @@ import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 class StockActions {
+    private Session session = HibernateConfig.INSTANCE.getSessionFactory().openSession();
+    private Transaction transaction = session.getTransaction();
+    private StockDao stockDao = new StockDao();
+
     private static boolean containsStock(final List<Stock> list, final String ticker) {
         return list.stream().anyMatch(o -> o.getTicker().equals(ticker));
     }
 
-    private static void removeStockFromList(final List<Stock> list, final String ticker) {
-        list.stream()
-                .filter(o -> o.getTicker().equals(ticker) && o.getQuantity() == 0)
-                .forEach(list::remove);
-    }
-
-    private static void settingStockQuantity(final List<Stock> list, String ticker, int quantity, int ratio) {
-        list.stream()
-                .filter(o -> o.getTicker().equals(ticker))
-                .forEach(o -> o.setQuantity(o.getQuantity() + (ratio * quantity)));
-    }
-
-    private static void settingParamsOfWallet(User user, int quantity, Stock stock, int ratio) {
-        if (ratio == -1) {
-            user.setStockValue(0);
-        } else {
-            user.setStockValue(user.getStockValue() + (ratio * (quantity * stock.getPrice())));
-        }
-        user.setBalanceAvailable(user.getBalanceAvailable() - (ratio * (quantity * stock.getPrice())));
-        user.setWalletValue(user.getStockValue() + user.getBalanceAvailable());
-    }
-
-    static void stockPurchaseParameters(int quantity, List<Stock> userStock, String ticker, Stock stock, User user) {
-//        Dodanie akcji do konta uzytkownika, gdy uzytkownik posiada akcje ktore chce kupic:
-        if (containsStock(userStock, ticker)) {
-            userStock.stream()
-                    .filter(o -> o.getTicker().equals(ticker))
-                    .forEach(o -> o.setAveragePurchasePrice(((o.getQuantity() * o.getPrice()) + (quantity * stock.getPrice())) / (o.getQuantity() + quantity)));
-            settingStockQuantity(userStock, ticker, quantity, 1);
-        } else { // Gdy nie posiada akcji:
-            userStock.add(stock);
-            stock.setQuantity(quantity);
-            stock.setAveragePurchasePrice(stock.getPrice());
-            user.setUserStock(userStock);
-        }
-        StockWIG20 stockWIG20 = new StockWIG20();
-        settingParamsOfWallet(user, quantity, stockWIG20.getByTicker(ticker), 1);
-    }
-
-    //Usuwanie sprzedanych akcji z konta uzytkownika
-    static void stockSellParameters(int quantity, int amountOfStock, List<Stock> userStock, User user, String ticker) {
-        if (quantity <= amountOfStock && quantity > 0) {
-            settingStockQuantity(userStock, ticker, quantity, -1);
-            StockWIG20 stockWIG20 = new StockWIG20();
-            settingParamsOfWallet(user, quantity, stockWIG20.getByTicker(ticker), -1);
-            removeStockFromList(userStock, ticker);
-            user.setUserStock(userStock);
-//            user.serializeUser(user);
-            System.out.println("Transakcja przebiegla pomyslnie.");
-        } else {
-            System.out.println("Nie posiadasz takiej ilosci akcji! Ilosc akcji w Twoim portfelu: " + amountOfStock);
-        }
-    }
-
     void stockPurchase(int quantity, String ticker, Long id) {
-        UserDao userDao = new UserDao();
-        User user = userDao.getUser(id);
-        List<Stock> userStock = new CopyOnWriteArrayList<>(user.getUserStock());
-        StockWIG20 stockWIG20 = new StockWIG20();
+        try {
+            transaction.begin();
+            User user = session.find(User.class, id);
+            List<Stock> userStock = new CopyOnWriteArrayList<>(user.getUserStock());
+            StockWIG20 stockWIG20 = new StockWIG20();
 
-        if (user.getBalanceAvailable() >= quantity * stockWIG20.getByTicker(ticker).getPrice() && quantity > 0) {
-            stockPurchaseParameters(quantity, userStock, ticker, stockWIG20.getByTicker(ticker), user);
-            System.out.println("Transakcja przebiegla pomyslnie.");
-//            user.serializeUser(finalUser);
-        } else {
-            int maxAmount = (int) Math.floor((user.getBalanceAvailable() / (stockWIG20.getByTicker(ticker).getPrice())));
-            System.out.println("Nie masz odpowiednich srodkow, maksymalna ilosc akcji jakie mozesz kupic: " + maxAmount);
+            if (user.getBalanceAvailable() >= quantity * stockWIG20.getByTicker(ticker).getPrice() && quantity > 0) {
+                Optional<Long> stockId = userStock.stream()
+                        .filter(o -> o.getTicker().equals(ticker))
+                        .map(Stock::getStockId)
+                        .findFirst();
+                Stock stock = session.find(Stock.class, stockId.get());
+
+                if (containsStock(userStock, ticker)) {
+                    stock.setAveragePurchasePrice((stock.getQuantity() * stock.getPrice()) + (quantity * stockWIG20.getByTicker(ticker).getPrice()) / (stock.getQuantity() + quantity));
+                    stock.setQuantity(stock.getQuantity() + quantity);
+                } else {
+                    stock.setQuantity(quantity);
+                    stock.setAveragePurchasePrice(stock.getPrice());
+                    user.addStockToList(stock);
+
+                    session.save(stock);
+
+                    stockDao.addStock(stock);
+                }
+                user.setBalanceAvailable(user.getBalanceAvailable() - (quantity * stock.getPrice()));
+                System.out.println("Transakcja przebiegla pomyslnie.");
+            } else {
+                int maxAmount = (int) Math.floor((user.getBalanceAvailable() / (stockWIG20.getByTicker(ticker).getPrice())));
+                System.out.println("Nie masz odpowiednich srodkow, maksymalna ilosc akcji jakie mozesz kupic: " + maxAmount);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
         }
     }
 
     void stockSell(int quantity, String ticker, Long id) {
-        UserDao userDao = new UserDao();
-        User user = userDao.getUser(id);
-        List<Stock> userStock = new CopyOnWriteArrayList<>(user.getUserStock());
-
-        if (StockActions.containsStock(userStock, ticker)) {
-
+        try {
+            transaction.begin();
+            User user = session.find(User.class, id);
+            List<Stock> userStock = new CopyOnWriteArrayList<>(user.getUserStock());
             Optional<Integer> amountOfStock = userStock.stream()
                     .filter(o -> o.getTicker().equals(ticker))
                     .map(Stock::getQuantity)
                     .findFirst();
 
-            stockSellParameters(quantity, amountOfStock.get(), userStock, user, ticker);
-        } else {
-            System.out.println("Nie posiadasz tych akcji!");
+            if (quantity > 0 && amountOfStock.isPresent()) {
+                if (amountOfStock.get() >= quantity) {
+                    Optional<Long> stockId = userStock.stream()
+                            .filter(o -> o.getTicker().equals(ticker))
+                            .map(Stock::getStockId)
+                            .findFirst();
+
+                    Stock stock = session.find(Stock.class, stockId.get());
+                    stock.setQuantity(stock.getQuantity() - quantity);
+
+                    StockWIG20 stockWIG20 = new StockWIG20();
+                    user.setBalanceAvailable(user.getBalanceAvailable() + (quantity * stockWIG20.getByTicker(ticker).getPrice()));
+
+                    if (amountOfStock.get() == quantity) {
+                        session.delete(stock);
+                        stockDao.deleteStock(stockId.get());
+                    }
+                    System.out.println("Transakcja przebiegla pomyslnie.");
+                } else {
+                    System.out.println("Nie posiadasz takiej ilosci akcji! Ilosc akcji w Twoim portfelu: " + amountOfStock);
+                }
+            } else {
+                System.out.println("Nie posiadasz tych akcji!");
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
         }
     }
 }
