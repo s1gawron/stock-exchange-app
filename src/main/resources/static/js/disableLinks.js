773d83a19731c2c57ce195e6522849e2c4290d4c
Sebastian <gawron241@gmail.com>
2020-03-20 18:04:26 +0100

    Zmiana zdania logicznego

--- src/main/resources/static/js/disableLinks.js
+++ src/main/resources/static/js/disableLinks.js
@@ -3,7 +3,7 @@ function showHide() {
     let dayOfWeek = date.getDay();
     let hourOfDay = date.getHours();
 
-    if (dayOfWeek === 6 || dayOfWeek === 0 || hourOfDay > 17 || hourOfDay < 9) {
+    if (dayOfWeek === 6 || dayOfWeek === 0 || hourOfDay < 9 || hourOfDay >= 17) {
         $(".buttonWrapper").click(function (e) {
             e.preventDefault();
             alert("Gielda czynna od Poniedzialku do Piatku w godzinach od 9 do 17. Zapraszamy!")
