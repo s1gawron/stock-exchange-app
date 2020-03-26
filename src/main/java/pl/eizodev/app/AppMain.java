5f06f8974ed24a792a892fc2f92945ed2504261a
Sebastian <gawron241@gmail.com>
2020-03-26 13:33:45 +0100

    Optymalizacja importow i usuniecie zbednego kodu

--- src/main/java/pl/eizodev/app/AppMain.java
+++ src/main/java/pl/eizodev/app/AppMain.java
@@ -3,22 +3,10 @@ package pl.eizodev.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import pl.eizodev.app.dao.UserDao;
-import pl.eizodev.app.entity.Stock;
-import pl.eizodev.app.entity.User;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class AppMain extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(AppMain.class, args);
-//        UserDao userDao = new UserDao();
-//        List<Stock> userStock = new ArrayList<>();
-//        User user = new User("testSebastian", LocalDate.now(), 0, 10000, 10000, 10000, userStock);
-////        userDao.addUser(user);
-
     }
 }
