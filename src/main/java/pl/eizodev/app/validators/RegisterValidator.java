faea53129f6ee1b28bffdb7831ece81fdb57f538
Sebastian <gawron241@gmail.com>
2020-04-07 10:01:02 +0200

    Walidator rejestracji

--- src/main/java/pl/eizodev/app/validators/RegisterValidator.java
+++ src/main/java/pl/eizodev/app/validators/RegisterValidator.java
@@ -1,65 +1,18 @@
 package pl.eizodev.app.validators;
 
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import pl.eizodev.app.HibernateConfig;
-
-import javax.persistence.Query;
-import java.math.BigInteger;
-import java.util.List;
+import org.springframework.validation.Errors;
+import pl.eizodev.app.entity.User;
 
 public class RegisterValidator {
-    public boolean userEmailExists(String email) {
-        Session session = HibernateConfig.INSTANCE.getSessionFactory().openSession();
-        Transaction transaction = session.getTransaction();
-        List<BigInteger> resultList = null;
-
-        try {
-            transaction.begin();
-            String sql = "SELECT id FROM user WHERE email = ?";
-            Query query = session.createNativeQuery(sql);
-            query.setParameter(1, email);
-            resultList = query.getResultList();
-            System.out.println(resultList);
-            transaction.commit();
-        } catch (Exception e) {
-            transaction.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-
-        if (resultList != null) {
-            return true;
-        } else {
-            return false;
+    public void userEmailExist(User user, Errors errors) {
+        if (user != null) {
+            errors.rejectValue("email", "error.userEmailExist");
         }
     }
 
-    public boolean userNameExists(String name) {
-        Session session = HibernateConfig.INSTANCE.getSessionFactory().openSession();
-        Transaction transaction = session.getTransaction();
-        List<BigInteger> resultList = null;
-
-        try {
-            transaction.begin();
-            String sql = "SELECT id FROM user WHERE name = ?";
-            Query query = session.createNativeQuery(sql);
-            query.setParameter(1, name);
-            resultList = query.getResultList();
-            System.out.println(resultList);
-            transaction.commit();
-        } catch (Exception e) {
-            transaction.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-
-        if (resultList != null) {
-            return true;
-        } else {
-            return false;
+    public void userNameExist(User user, Errors errors) {
+        if (user != null) {
+            errors.rejectValue("name", "error.userNameExist");
         }
     }
 }
