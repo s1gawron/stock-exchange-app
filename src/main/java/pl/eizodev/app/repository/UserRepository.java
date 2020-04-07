1974a27ec65e43c56cab733aec591362eddd52fe
Sebastian <gawron241@gmail.com>
2020-04-07 10:00:51 +0200

    Repozytorium usera

---
+++ src/main/java/pl/eizodev/app/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package pl.eizodev.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.eizodev.app.entity.User;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByName(String name);
+    public User findByEmail(String email);
+}
