6ded70de6a0707ae895c5b4cc750d80687577a82
Sebastian <gawron241@gmail.com>
2020-04-08 09:07:43 +0200

    Uzywanie UserService do wyswietlania danych

--- src/main/java/pl/eizodev/app/controllers/MainController.java
+++ src/main/java/pl/eizodev/app/controllers/MainController.java
@@ -1,26 +1,27 @@
 package pl.eizodev.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pl.eizodev.app.StockActions;
 import pl.eizodev.app.StockWIG20;
-import pl.eizodev.app.dao.StockDao;
-import pl.eizodev.app.dao.UserDao;
 import pl.eizodev.app.entity.User;
+import pl.eizodev.app.services.UserService;
 
 @Controller
 @RequestMapping("/stock")
 public class MainController {
     private StockWIG20 stockWIG20 = new StockWIG20();
-    private UserDao userDao = new UserDao();
     private User user;
-    private StockDao stockDao = new StockDao();
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/mainView")
     public String mainView(Model model) {
-        userDao.updateUser(1L);
-        user = userDao.getUser(1L);
+//        userDao.updateUser(1L);
+        user = userService.findById(1L).get();
         model.addAttribute("user", user);
 
         return "index";
@@ -28,7 +29,7 @@ public class MainController {
 
     @GetMapping("/statsWIG20")
     public String statsWIG20(Model model) {
-        user = userDao.getUser(1L);
+        user = userService.findById(1L).get();
         model.addAttribute("stocks", stockWIG20.getAll());
         model.addAttribute("user", user);
 
@@ -37,13 +38,13 @@ public class MainController {
 
     @GetMapping("/myWallet")
     public String myWallet(Model model) {
-        userDao.updateUser(1L);
+//        userDao.updateUser(1L);
 
-        if (!user.getUserStock().isEmpty()) {
-            stockDao.updateStock(1L);
-        }
+//        if (!user.getUserStock().isEmpty()) {
+//            stockDao.updateStock(1L);
+//        }
 
-        user = userDao.getUser(1L);
+        user = userService.findById(1L).get();
 
         model.addAttribute("user", user);
         model.addAttribute("userStock", user.getUserStock());
@@ -53,7 +54,7 @@ public class MainController {
 
     @GetMapping("/order")
     public String orderForm(@RequestParam("ticker") String ticker, Model model) {
-        user = userDao.getUser(1L);
+        user = userService.findById(1L).get();
         model.addAttribute("user", user);
         model.addAttribute("stock", stockWIG20.getByTicker(ticker));
 
@@ -67,7 +68,7 @@ public class MainController {
             @RequestParam(value = "quantity") int quantity
     ) {
         StockActions stockActions = new StockActions();
-        user = userDao.getUser(1L);
+        user = userService.findById(1L).get();
 
         if (action.equals("buy")) {
             stockActions.stockPurchase(quantity, ticker, user.getUserId());
