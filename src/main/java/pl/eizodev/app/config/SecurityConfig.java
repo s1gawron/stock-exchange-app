bc70b1a79b57c7c8652ee6e9f30513cd62de69d7
Sebastian <gawron241@gmail.com>
2020-04-09 19:01:20 +0200

    Konfiguracja spring security

---
+++ src/main/java/pl/eizodev/app/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package pl.eizodev.app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/stock/mainView").authenticated()
+                .antMatchers("/stock/myWallet").authenticated()
+                .antMatchers("/stock/order").authenticated()
+                .antMatchers("/stock/process-order").authenticated()
+                .anyRequest().permitAll()
+//                .and()
+//                .requiresChannel()
+//                .antMatchers("/user/register").requiresSecure()
+//                .antMatchers("/user/login").requiresSecure()
+//                .antMatchers("/user/add-user").requiresSecure()
+                .and()
+                .formLogin()
+//                .loginPage("/user/login")
+                .defaultSuccessUrl("/stock/mainView")
+                .failureUrl("/login?error=true");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, true from users where username=?")
+                .authoritiesByUsernameQuery("select username, role from users where username=?")
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
+}
