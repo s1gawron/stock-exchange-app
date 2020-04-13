be3840773a8c11855066acce33cf88f2250f75d3
Sebastian <gawron241@gmail.com>
2020-04-13 13:12:05 +0200

    Zmiany w zwiazku z dziedziczeniem

--- src/main/java/pl/eizodev/app/controllers/MainController.java
+++ src/main/java/pl/eizodev/app/controllers/MainController.java
@@ -5,10 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 //import pl.eizodev.app.StockActions;
-import pl.eizodev.app.StockWIG20;
+import pl.eizodev.app.entity.Stock;
+import pl.eizodev.app.stocks.StockWIG20;
 import pl.eizodev.app.entity.User;
 import pl.eizodev.app.services.UserService;
 import pl.eizodev.app.utilities.UserUtilities;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/stock")
@@ -35,7 +38,7 @@ public class MainController {
         String username = UserUtilities.getLoggedUser();
         user = userService.findByName(username);
 
-        model.addAttribute("stocks", stockWIG20.getAll());
+        model.addAttribute("stocks", stockWIG20.getAllStocksWIG20());
         model.addAttribute("user", user);
 
         return "statsWIG20";
@@ -62,9 +65,10 @@ public class MainController {
     public String orderForm(@RequestParam("ticker") String ticker, Model model) {
         String username = UserUtilities.getLoggedUser();
         user = userService.findByName(username);
+        List<Stock> stocks = stockWIG20.getAllStocksWIG20();
 
         model.addAttribute("user", user);
-        model.addAttribute("stock", stockWIG20.getByTicker(ticker));
+        model.addAttribute("stock", stockWIG20.getByTicker(stocks, ticker));
 
         return "orderform";
     }
