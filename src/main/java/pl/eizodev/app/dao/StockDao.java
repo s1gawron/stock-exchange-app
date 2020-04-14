05175872948a214503c1e18a67dbe1d67a143c84
Sebastian <gawron241@gmail.com>
2020-04-14 10:13:31 +0200

    Komentarz do blednych lub nieuzywanych metod

--- src/main/java/pl/eizodev/app/dao/StockDao.java
+++ src/main/java/pl/eizodev/app/dao/StockDao.java
@@ -1,68 +1,36 @@
-package pl.eizodev.app.dao;
-
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import pl.eizodev.app.HibernateConfig;
-import pl.eizodev.app.StockWIG20;
-import pl.eizodev.app.entity.Stock;
-import pl.eizodev.app.entity.User;
-
-import java.util.List;
-
-public class StockDao {
-    public void addStock(Stock stock) {
-        Session session = HibernateConfig.INSTANCE.getSessionFactory().openSession();
-        Transaction transaction = session.getTransaction();
-
-        try {
-            transaction.begin();
-            session.save(stock);
-            transaction.commit();
-        } catch (Exception e) {
-            transaction.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-    }
-
-    public void updateStock(Long id) {
-        Session session = HibernateConfig.INSTANCE.getSessionFactory().openSession();
-        Transaction transaction = session.getTransaction();
-
-        try {
-            transaction.begin();
-            User user = session.find(User.class, id);
-            List<Stock> userStocks = user.getUserStock();
-            StockWIG20 stockWIG20 = new StockWIG20();
-
-            for (Stock stock : userStocks) {
-                stock.setPrice(stockWIG20.getByTicker(stock.getTicker()).getPrice());
-                stock.setProfitLoss((stock.getPrice() - stock.getAveragePurchasePrice()) * stock.getQuantity());
-            }
-            transaction.commit();
-        } catch (Exception e) {
-            transaction.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-    }
-
-    public void deleteStock(Long id) {
-        Session session = HibernateConfig.INSTANCE.getSessionFactory().openSession();
-        Transaction transaction = session.getTransaction();
-
-        try {
-            transaction.begin();
-            Stock stock = session.find(Stock.class, id);
-            session.delete(stock);
-            transaction.commit();
-        } catch (Exception e) {
-            transaction.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-    }
-}
+//package pl.eizodev.app.dao;
+//
+//import org.hibernate.Session;
+//import org.hibernate.Transaction;
+//import pl.eizodev.app.HibernateConfig;
+//import pl.eizodev.app.StockWIG20;
+//import pl.eizodev.app.entity.Stock;
+//import pl.eizodev.app.entity.User;
+//
+//import java.util.List;
+//
+//public class StockDao {
+//    public void updateStock(Long id) {
+//        Session session = HibernateConfig.INSTANCE.getSessionFactory().openSession();
+//        Transaction transaction = session.getTransaction();
+//
+//        try {
+//            transaction.begin();
+//            User user = session.find(User.class, id);
+//            List<Stock> userStocks = user.getUserStock();
+//            StockWIG20 stockWIG20 = new StockWIG20();
+//
+//            for (Stock stock : userStocks) {
+//                stock.setPrice(stockWIG20.getByTicker(stock.getTicker()).getPrice());
+//                stock.setChange(stockWIG20.getByTicker(stock.getTicker()).getChange());
+//                stock.setProfitLoss((stock.getPrice() - stock.getAveragePurchasePrice()) * stock.getQuantity());
+//            }
+//            transaction.commit();
+//        } catch (Exception e) {
+//            transaction.rollback();
+//            e.printStackTrace();
+//        } finally {
+//            session.close();
+//        }
+//    }
+//}
--- src/main/java/pl/eizodev/app/dao/UserDao.java
+++ src/main/java/pl/eizodev/app/dao/UserDao.java
@@ -1,86 +1,45 @@
-package pl.eizodev.app.dao;
-
-import org.hibernate.Hibernate;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import pl.eizodev.app.HibernateConfig;
-import pl.eizodev.app.entity.Stock;
-import pl.eizodev.app.entity.User;
-
-import java.time.LocalDate;
-import java.util.List;
-
-public class UserDao {
-    public User getUser(Long id) {
-        Session session = HibernateConfig.INSTANCE.getSessionFactory().openSession();
-        User user = session.get(User.class, id);
-        Hibernate.initialize(user.getUserStock());
-        session.close();
-        return user;
-    }
-
-    public void addUser(User user) {
-        Session session = HibernateConfig.INSTANCE.getSessionFactory().openSession();
-        Transaction transaction = session.getTransaction();
-
-        try {
-            transaction.begin();
-            session.save(user);
-            transaction.commit();
-        } catch (Exception e) {
-            transaction.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-    }
-
-    public void updateUser(Long id) {
-        Session session = HibernateConfig.INSTANCE.getSessionFactory().openSession();
-        Transaction transaction = session.getTransaction();
-
-        try {
-            transaction.begin();
-            User user = session.find(User.class, id);
-            List<Stock> userStocks = user.getUserStock();
-            float stockValue = 0;
-
-            for (Stock stock : userStocks) {
-                stockValue += (stock.getQuantity() * stock.getPrice());
-            }
-
-            user.setStockValue(stockValue);
-
-            if (LocalDate.now().isAfter(user.getUserUpdate())) {
-                user.setPrevWalletValue(user.getWalletValue());
-            }
-
-            user.setUserUpdate(LocalDate.now());
-            user.setWalletValue(user.getStockValue() + user.getBalanceAvailable());
-            user.setWalletPercChange((user.getWalletValue() - user.getPrevWalletValue()) / user.getPrevWalletValue());
-            transaction.commit();
-        } catch (Exception e) {
-            transaction.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-    }
-
-    public void deleteUser(Long id) {
-        Session session = HibernateConfig.INSTANCE.getSessionFactory().openSession();
-        Transaction transaction = session.getTransaction();
-
-        try {
-            transaction.begin();
-            User user = session.find(User.class, id);
-            session.delete(user);
-            transaction.commit();
-        } catch (Exception e) {
-            transaction.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-    }
-}
+//package pl.eizodev.app.dao;
+//
+//import org.hibernate.Hibernate;
+//import org.hibernate.Session;
+//import org.hibernate.Transaction;
+//import pl.eizodev.app.HibernateConfig;
+//import pl.eizodev.app.entity.Stock;
+//import pl.eizodev.app.entity.User;
+//
+//import java.time.LocalDate;
+//import java.util.List;
+//
+//public class UserDao {
+//    public void updateUser(Long id) {
+//        Session session = HibernateConfig.INSTANCE.getSessionFactory().openSession();
+//        Transaction transaction = session.getTransaction();
+//
+//        try {
+//            transaction.begin();
+//            User user = session.find(User.class, id);
+//            List<Stock> userStocks = user.getUserStock();
+//            float stockValue = 0;
+//
+//            for (Stock stock : userStocks) {
+//                stockValue += (stock.getQuantity() * stock.getPrice());
+//            }
+//
+//            user.setStockValue(stockValue);
+//
+//            if (LocalDate.now().isAfter(user.getUserUpdate())) {
+//                user.setPrevWalletValue(user.getWalletValue());
+//            }
+//
+//            user.setUserUpdate(LocalDate.now());
+//            user.setWalletValue(user.getStockValue() + user.getBalanceAvailable());
+//            user.setWalletPercChange((user.getWalletValue() - user.getPrevWalletValue()) / user.getPrevWalletValue());
+//            transaction.commit();
+//        } catch (Exception e) {
+//            transaction.rollback();
+//            e.printStackTrace();
+//        } finally {
+//            session.close();
+//        }
+//    }
+//}
