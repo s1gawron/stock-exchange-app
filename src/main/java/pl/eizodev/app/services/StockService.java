9cd6d832d95ce6e9934254301b32bc3f34326e73
Sebastian <gawron241@gmail.com>
2020-04-16 11:21:09 +0200

    Dodanie wyszukiwania akcji po ID i update akcji na koncie uzytkownika

--- src/main/java/pl/eizodev/app/services/StockService.java
+++ src/main/java/pl/eizodev/app/services/StockService.java
@@ -1,8 +1,11 @@
 package pl.eizodev.app.services;
 
 import pl.eizodev.app.entity.Stock;
+import pl.eizodev.app.entity.User;
 
 public interface StockService {
+    Stock findById(Long id);
     void saveStock(Stock stock);
+    void updateStock(User user);
     void deleteStock(Long id);
 }
--- src/main/java/pl/eizodev/app/services/StockServiceImpl.java
+++ src/main/java/pl/eizodev/app/services/StockServiceImpl.java
@@ -3,9 +3,12 @@ package pl.eizodev.app.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.eizodev.app.entity.Stock;
+import pl.eizodev.app.entity.User;
 import pl.eizodev.app.repository.StockRepository;
+import pl.eizodev.app.stocks.StockWIG20;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
@@ -15,8 +18,25 @@ public class StockServiceImpl implements StockService {
     private StockRepository stockRepository;
 
     @Override
+    public Stock findById(Long id) {
+        return stockRepository.findByStockId(id);
+    }
+
+    @Override
     public void saveStock(Stock stock) {
         stockRepository.save(stock);
+    }
+
+    @Override
+    public void updateStock(User user) {
+        List<Stock> userStocks = user.getUserStock();
+        StockWIG20 stockWIG20 = new StockWIG20();
+
+        for (Stock stock : userStocks) {
+            stock.setPrice(stockWIG20.getByTicker(stockWIG20.getAllStocksWIG20(), stock.getTicker()).getPrice());
+            stock.setChange(stockWIG20.getByTicker(stockWIG20.getAllStocksWIG20(), stock.getTicker()).getChange());
+            stock.setProfitLoss((stock.getPrice() - stock.getAveragePurchasePrice()) * stock.getQuantity());
+        }
     }
 
     @Override
