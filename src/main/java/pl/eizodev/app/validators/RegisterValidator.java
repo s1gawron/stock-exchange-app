1af959199680ccf29b590b77a8b74dc59d17414f
Sebastian <gawron241@gmail.com>
2020-04-21 15:32:06 +0200

    Wymaganie bardziej zlozonego hasla

--- src/main/java/pl/eizodev/app/validators/RegisterValidator.java
+++ src/main/java/pl/eizodev/app/validators/RegisterValidator.java
@@ -3,6 +3,9 @@ package pl.eizodev.app.validators;
 import org.springframework.validation.Errors;
 import pl.eizodev.app.entity.User;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegisterValidator {
     public void userEmailExist(User user, Errors errors) {
         if (user != null) {
@@ -13,6 +16,15 @@ public class RegisterValidator {
     public void userNameExist(User user, Errors errors) {
         if (user != null) {
             errors.rejectValue("name", "error.userNameExist");
+        }
+    }
+
+    public void invalidPasswordPattern(User user, Errors errors) {
+        Pattern pattern = Pattern.compile("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$*])(?!.*\\s).{8,12}$");
+        Matcher matcher = pattern.matcher(user.getPassword());
+
+        if (!matcher.matches()) {
+            errors.rejectValue("password", "error.invalidPasswordPattern");
         }
     }
 }
