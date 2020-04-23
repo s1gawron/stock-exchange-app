1e0cd39e0f1e95edb4fa7361460f3220b4d3226e
Sebastian <gawron241@gmail.com>
2020-04-23 17:33:34 +0200

    Szukanie akcji po uzytkowniku i symbolu akcji

--- src/main/java/pl/eizodev/app/repository/StockRepository.java
+++ src/main/java/pl/eizodev/app/repository/StockRepository.java
@@ -2,7 +2,9 @@ package pl.eizodev.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.eizodev.app.entity.Stock;
+import pl.eizodev.app.entity.User;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
     Stock findByStockId(Long id);
+    Stock findByUserAndTicker(User user, String ticker);
 }
