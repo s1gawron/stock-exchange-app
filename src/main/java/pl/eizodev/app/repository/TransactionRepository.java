a41a9851302793885ed61ad68c376e63da914195
Sebastian <gawron241@gmail.com>
2020-04-23 17:33:48 +0200

    Transaction repo

---
+++ src/main/java/pl/eizodev/app/repository/TransactionRepository.java
@@ -0,0 +1,11 @@
+package pl.eizodev.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.eizodev.app.entity.Transaction;
+
+import java.util.Optional;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    Transaction findByTransactionId(Long id);
+    Optional<Transaction> findByTransactionTypeAndTransactionStockTicker(String transactionType, String ticker);
+}
