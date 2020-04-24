c03163f5a012e3b2dc47a45a0067d106782c74a1
Sebastian <gawron241@gmail.com>
2020-04-24 09:38:00 +0200

    Przeniesienie walidacji transakcji, zmiana sposobu ich przeprowadzania oraz przeniesienie klasy do osobnego katalogu

---
+++ src/main/java/pl/eizodev/app/soloUser/StockActions.java
@@ -0,0 +1,56 @@
+package pl.eizodev.app.soloUser;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.eizodev.app.entity.Stock;
+import pl.eizodev.app.entity.User;
+import pl.eizodev.app.services.StockService;
+import pl.eizodev.app.services.UserService;
+import pl.eizodev.app.stocks.StockWIG20;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Service
+public class StockActions {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private StockService stockService;
+
+    public void stockPurchase(int quantity, String ticker, Long userId) {
+        User user = userService.findById(userId).get();
+        Stock stock = stockService.findByUserAndStockTicker(user, ticker);
+        StockWIG20 stockWIG20 = new StockWIG20();
+
+        Stock newStock = stockWIG20.getByTicker(stockWIG20.getAllStocksWIG20(), ticker);
+
+        if (stock != null) {
+            stock.setAveragePurchasePrice(((stock.getQuantity() * stock.getPrice()) + (quantity * stockWIG20.getByTicker(stockWIG20.getAllStocksWIG20(), ticker).getPrice())) / (stock.getQuantity() + quantity));
+            stock.setQuantity(stock.getQuantity() + quantity);
+        } else {
+            newStock.setQuantity(quantity);
+            newStock.setAveragePurchasePrice(newStock.getPrice());
+            newStock.setUser(user);
+            user.getUserStock().add(newStock);
+            stockService.saveStock(newStock);
+        }
+        user.setBalanceAvailable(user.getBalanceAvailable() - (quantity * newStock.getPrice()));
+    }
+
+    public void stockSell(int quantity, String ticker, Long userId) {
+        User user = userService.findById(userId).get();
+        Stock stock = stockService.findByUserAndStockTicker(user, ticker);
+
+        StockWIG20 stockWIG20 = new StockWIG20();
+        user.setBalanceAvailable(user.getBalanceAvailable() + (quantity * stockWIG20.getByTicker(stockWIG20.getAllStocksWIG20(), ticker).getPrice()));
+
+        if (stock.getQuantity() == quantity) {
+            stockService.deleteStock(stock.getStockId());
+        } else {
+            stock.setQuantity(stock.getQuantity() - quantity);
+        }
+    }
+}
