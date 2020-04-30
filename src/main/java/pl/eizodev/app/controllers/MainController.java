5d6498bb2f4aeada44d0b9da7893e4cb62a3b5a3
Sebastian <gawron241@gmail.com>
2020-04-30 09:42:13 +0200

    Przeniesienie obslugi formularza transakcji do innej klasy

--- src/main/java/pl/eizodev/app/controllers/MainController.java
+++ src/main/java/pl/eizodev/app/controllers/MainController.java
@@ -3,10 +3,8 @@ package pl.eizodev.app.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import pl.eizodev.app.soloUser.StockActions;
-import pl.eizodev.app.entity.Transaction;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pl.eizodev.app.entity.User;
 import pl.eizodev.app.services.StockService;
 import pl.eizodev.app.services.UserService;
@@ -24,9 +22,6 @@ public class MainController {
 
     @Autowired
     private StockService stockService;
-
-    @Autowired
-    private StockActions stockActions;
 
     @GetMapping("/mainView")
     public String mainView(Model model) {
@@ -65,44 +60,5 @@ public class MainController {
         model.addAttribute("userStock", user.getUserStock());
 
         return "mywallet";
-    }
-
-    @GetMapping("/order")
-    public String orderForm(@RequestParam("ticker") String ticker, Model model) {
-        String username = UserUtilities.getLoggedUser();
-        user = userService.findByName(username);
-
-        model.addAttribute("user", user);
-        model.addAttribute("stock", stockWIG20.getByTicker(stockWIG20.getAllStocksWIG20(), ticker));
-
-        return "orderform";
-    }
-
-    @PostMapping("/process-order")
-    public String processOrderForm(
-            @RequestParam(value = "ticker") String ticker,
-            @RequestParam(value = "action") String action,
-            @RequestParam(value = "quantity") int quantity,
-            @ModelAttribute Transaction transaction,
-            BindingResult result
-    ) {
-
-        String returnPage = null;
-
-//        new TransactionValidator().hasEnoughMoney(transaction, result);
-//        new TransactionValidator().hasEnoughStock(transaction, result);
-
-//        if (result.hasErrors()) {
-//            returnPage = "order";
-//        } else {
-            if (action.equals("buy")) {
-                stockActions.stockPurchase(quantity, ticker, user.getUserId());
-            } else if (action.equals("sell")) {
-                stockActions.stockSell(quantity, ticker, user.getUserId());
-            }
-            returnPage = "redirect:/stock/myWallet";
-//        }
-
-        return returnPage;
     }
 }
