951eefc6c26281d079190bba53d939a93d6858f2
Sebastian <gawron241@gmail.com>
2020-05-01 10:58:29 +0200

    Aktualizacja uzytkownika po usernam'ie w celu lepszej optymalizacji kontrolera

--- src/main/java/pl/eizodev/app/services/UserService.java
+++ src/main/java/pl/eizodev/app/services/UserService.java
@@ -9,6 +9,6 @@ public interface UserService {
     User findByName(String name);
     User findByEmail(String email);
     void saveUser(User user);
-    void updateUser(User user);
+    void updateUser(String username);
     void deleteUser(String email);
 }
