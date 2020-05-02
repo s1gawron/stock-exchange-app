6b238c77b3e7f68333e619a3139aa4d1a2f03495
Sebastian <gawron241@gmail.com>
2020-05-02 09:37:53 +0200

    Chwilowe wylaczenie blokowania linkow

--- src/main/resources/static/js/disableLinks.js
+++ src/main/resources/static/js/disableLinks.js
@@ -1,16 +1,16 @@
-function showHide() {
-    let date = new Date();
-    let dayOfWeek = date.getDay();
-    let hourOfDay = date.getHours();
-
-    if (dayOfWeek === 6 || dayOfWeek === 0 || hourOfDay < 9 || hourOfDay >= 17) {
-        $(".buttonWrapper").click(function (e) {
-            e.preventDefault();
-            alert("Gielda czynna od Poniedzialku do Piatku w godzinach od 9 do 17. Zapraszamy!")
-        });
-    }
-}
-
-$(document).ready(function () {
-    showHide();
-});
+// function showHide() {
+//     let date = new Date();
+//     let dayOfWeek = date.getDay();
+//     let hourOfDay = date.getHours();
+//
+//     if (dayOfWeek === 6 || dayOfWeek === 0 || hourOfDay < 9 || hourOfDay >= 17) {
+//         $(".buttonWrapper").click(function (e) {
+//             e.preventDefault();
+//             alert("Gielda czynna od Poniedzialku do Piatku w godzinach od 9 do 17. Zapraszamy!")
+//         });
+//     }
+// }
+//
+// $(document).ready(function () {
+//     showHide();
+// });
