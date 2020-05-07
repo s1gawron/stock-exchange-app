d688080ffd5c122ae0c13660decdac44d68e4b5e
Sebastian <gawron241@gmail.com>
2020-05-07 20:43:12 +0200

    Dodanie zapomnianych adnotacji Repository

--- src/main/java/pl/eizodev/app/repository/StockRepository.java
+++ src/main/java/pl/eizodev/app/repository/StockRepository.java
@@ -1,9 +1,11 @@
 package pl.eizodev.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.eizodev.app.entity.Stock;
 import pl.eizodev.app.entity.User;
 
+@Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
     Stock findByStockId(Long id);
     Stock findByUserAndTicker(User user, String ticker);
--- src/main/java/pl/eizodev/app/repository/TransactionRepository.java
+++ src/main/java/pl/eizodev/app/repository/TransactionRepository.java
@@ -1,10 +1,12 @@
 package pl.eizodev.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.eizodev.app.entity.Transaction;
 
 import java.util.Optional;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     Transaction findByTransactionId(Long id);
     Optional<Transaction> findByTransactionTypeAndStockTicker(String transactionType, String ticker);
