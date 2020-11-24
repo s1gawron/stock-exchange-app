0abb2050cd914ec074f1b053671c8bac9c7c709e
Sebastian <gawron241@gmail.com>
2020-11-24 13:44:03 +0100

    Getting rid of noise caused by single implementation interfaces

--- src/main/java/pl/eizodev/app/services/TransactionService.java
+++ src/main/java/pl/eizodev/app/services/TransactionService.java
@@ -1,11 +1,39 @@
 package pl.eizodev.app.services;
 
+import org.springframework.stereotype.Service;
 import pl.eizodev.app.entities.Transaction;
+import pl.eizodev.app.repositories.TransactionRepository;
 
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
+import java.util.Optional;
 
-public interface TransactionService {
-    void addTransaction(Transaction transaction);
-    void performTransaction(String transactionType, String ticker, Long userId, BigDecimal price, int quantity);
-    void deleteTransaction(Long id);
+@Service
+@Transactional
+class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public void addTransaction(Transaction transaction) {
+        transactionRepository.save(transaction);
+    }
+
+    public void performTransaction(String transactionType, String ticker, Long userId, BigDecimal price, int quantity) {
+        Optional<Transaction> existInDB = transactionRepository.findByTransactionTypeAndStockTicker(transactionType, ticker);
+
+        if (existInDB.isPresent()) {
+
+        } else {
+            Transaction transaction = new Transaction(transactionType, ticker, price, quantity, userId);
+            addTransaction(transaction);
+        }
+    }
+
+    public void deleteTransaction(Long id) {
+        transactionRepository.delete(transactionRepository.findByTransactionId(id));
+    }
 }
