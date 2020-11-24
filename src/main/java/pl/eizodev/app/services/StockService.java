018fe877af589efeb9589115ef6b8d3dfaa682a1
Sebastian <gawron241@gmail.com>
2020-11-24 13:44:03 +0100

    Getting rid of noise caused by single implementation interfaces

--- src/main/java/pl/eizodev/app/services/StockService.java
+++ src/main/java/pl/eizodev/app/services/StockService.java
@@ -1,9 +1,73 @@
 package pl.eizodev.app.services;
 
+import org.springframework.stereotype.Service;
 import pl.eizodev.app.entities.Stock;
+import pl.eizodev.app.entities.User;
+import pl.eizodev.app.repositories.StockRepository;
+import pl.eizodev.app.repositories.UserRepository;
+import pl.eizodev.app.stockstats.StockFactory;
 
-public interface StockService {
-    void saveStock(Stock stock);
-    void updateStock(String username);
-    void deleteStock(Long id);
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class StockService {
+
+    private final StockRepository stockRepository;
+    private final UserRepository userRepository;
+
+    public StockService(StockRepository stockRepository, UserRepository userRepository) {
+        this.stockRepository = stockRepository;
+        this.userRepository = userRepository;
+    }
+
+    public void saveStock(Stock stock) {
+        stockRepository.save(stock);
+    }
+
+    public void updateStock(String username) {
+        Optional<User> userOptional = userRepository.findByName(username);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            List<Stock> userStocks = user.getUserStock();
+
+            if (!userStocks.isEmpty()) {
+                StockFactory stockFactory = new StockFactory();
+                List<Stock> WIG20 = stockFactory.getAllStocksFromGivenIndex("WIG20");
+                List<Stock> WIG40 = stockFactory.getAllStocksFromGivenIndex("WIG40");
+                List<Stock> WIG80 = stockFactory.getAllStocksFromGivenIndex("WIG80");
+
+                for (Stock stock : userStocks) {
+                    Optional<Stock> temp = Optional.empty();
+
+                    switch (stock.getIndex()) {
+                        case "WIG20":
+                            temp = stockFactory.getByTicker(WIG20, stock.getTicker());
+                            break;
+                        case "WIG40":
+                            temp = stockFactory.getByTicker(WIG40, stock.getTicker());
+                            break;
+                        case "WIG80":
+                            temp = stockFactory.getByTicker(WIG80, stock.getTicker());
+                            break;
+                    }
+
+                    if (temp.isPresent()) {
+                        stock.setPrice(temp.get().getPrice());
+                        stock.setChange(temp.get().getChange());
+                        stock.setProfitLoss((stock.getPrice().subtract(stock.getAveragePurchasePrice())).multiply(BigDecimal.valueOf(stock.getQuantity())));
+                    }
+                }
+            }
+        }
+    }
+
+    public void deleteStock(Long id) {
+        Optional<Stock> stockOptional = stockRepository.findByStockId(id);
+        stockOptional.ifPresent(stockRepository::delete);
+    }
 }
--- src/main/java/pl/eizodev/app/services/UserService.java
+++ src/main/java/pl/eizodev/app/services/UserService.java
@@ -1,9 +1,69 @@
 package pl.eizodev.app.services;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import pl.eizodev.app.entities.Stock;
 import pl.eizodev.app.entities.User;
+import pl.eizodev.app.repositories.UserRepository;
 
-public interface UserService {
-    void saveUser(User user);
-    void updateUser(String username);
-    void deleteUser(String email);
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public @Transactional
+class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void saveUser(User user) {
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        user.setActive(1);
+        user.setUserUpdate(LocalDate.now());
+        user.setRole("USER");
+        user.setWalletValue(user.getBalanceAvailable());
+        user.setPrevWalletValue(user.getWalletValue());
+
+        userRepository.save(user);
+    }
+
+    public void updateUser(String username) {
+        Optional<User> userOptional = userRepository.findByName(username);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            List<Stock> userStocks = user.getUserStock();
+
+            if (!userStocks.isEmpty()) {
+                BigDecimal stockValue = new BigDecimal(0);
+
+                for (Stock stock : userStocks) {
+                    stockValue = stockValue.add(stock.getPrice().multiply(BigDecimal.valueOf(stock.getQuantity())));
+                }
+                user.setStockValue(stockValue);
+            } else {
+                user.setStockValue(new BigDecimal(0));
+            }
+
+            if (LocalDate.now().isAfter(user.getUserUpdate())) {
+                user.setPrevWalletValue(user.getWalletValue());
+            }
+
+            user.setUserUpdate(LocalDate.now());
+            user.setWalletValue(user.getStockValue().add(user.getBalanceAvailable()));
+            user.setWalletPercChange(((user.getWalletValue().subtract(user.getPrevWalletValue())).divide(user.getPrevWalletValue(), RoundingMode.HALF_DOWN)).multiply(BigDecimal.valueOf(100)));
+        }
+    }
+
+    public void deleteUser(String email) {
+        Optional<User> userOptional = userRepository.findByEmail(email);
+        userOptional.ifPresent(userRepository::delete);
+    }
 }
