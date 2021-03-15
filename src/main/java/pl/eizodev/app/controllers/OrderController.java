fa56e7a5106c9c496cddb41a4746714f483fc0ad
Sebastian <gawron241@gmail.com>
2021-03-15 15:44:54 +0100

    Fixing errors caused by same exception passed to exception handler methods

--- src/main/java/pl/eizodev/app/controllers/OrderController.java
+++ src/main/java/pl/eizodev/app/controllers/OrderController.java
@@ -1,10 +1,7 @@
 package pl.eizodev.app.controllers;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.eizodev.app.dtos.TransactionDTO;
 import pl.eizodev.app.offlineuser.OfflineStockTransaction;
 import pl.eizodev.app.services.models.TransactionResult;
@@ -12,7 +9,7 @@ import pl.eizodev.app.services.models.TransactionResult;
 @RestController
 @RequestMapping("order")
 @AllArgsConstructor
-class OrderController {
+class OrderController extends AbstractErrorHandlerController {
 
     private final OfflineStockTransaction offlineStockTransaction;
 
