7e47f186de6f115cd09646af16b10f0cfc3386ec
Sebastian <gawron241@gmail.com>
2022-04-13 08:34:58 +0200

    Refactoring user wallet update method + implementing tests

--- src/main/java/pl/eizodev/app/user/service/UserWalletService.java
+++ src/main/java/pl/eizodev/app/user/service/UserWalletService.java
@@ -6,14 +6,14 @@ import org.springframework.transaction.annotation.Transactional;
 import pl.eizodev.app.stock.dataprovider.StockDataProvider;
 import pl.eizodev.app.user.dto.UserWalletDTO;
 import pl.eizodev.app.user.exception.UserWalletNotFoundException;
-import pl.eizodev.app.user.model.UserStock;
 import pl.eizodev.app.user.model.UserWallet;
 import pl.eizodev.app.user.repository.UserWalletRepository;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.Clock;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Service
 @AllArgsConstructor
@@ -23,52 +23,37 @@ public class UserWalletService {
 
     private final StockDataProvider stockDataProvider;
 
+    private final Clock clock;
+
     @Transactional
     public UserWalletDTO updateAndGetUserWallet(final String username) {
         final UserWallet userWallet = userWalletRepository.findByUser_Username(username)
             .orElseThrow(() -> UserWalletNotFoundException.create(username));
 
-        final List<UserStock> userStockList = userWallet.getUserStock();
+        final AtomicReference<BigDecimal> stockValue = new AtomicReference<>(BigDecimal.ZERO);
 
-        if (userStockList.isEmpty()) {
-            userWallet.setStockValue(BigDecimal.ZERO);
-        } else {
-            BigDecimal stockValue = new BigDecimal(0);
+        userWallet.getUserStock().forEach(userStock -> {
+            final String ticker = userStock.getTicker();
+            final BigDecimal stockPrice = stockDataProvider.getStockData(ticker).getStockQuote().getCurrentPrice();
+            final BigDecimal stockQuantityBigDecimal = BigDecimal.valueOf(userStock.getQuantity());
+            final BigDecimal walletValueOfSpecificStock = stockPrice.multiply(stockQuantityBigDecimal);
 
-            for (UserStock userStock : userStockList) {
-                final String ticker = userStock.getTicker();
-                final BigDecimal stockPrice = stockDataProvider.getStockData(ticker).getStockQuote().getCurrentPrice();
-                final BigDecimal stockQuantityBigDecimal = BigDecimal.valueOf(userStock.getQuantity());
+            stockValue.set(stockValue.get().add(walletValueOfSpecificStock));
+        });
 
-                stockValue = stockValue.add(stockPrice.multiply(stockQuantityBigDecimal));
-            }
-
-            userWallet.setStockValue(stockValue);
-        }
-
-        final int currentDayOfYear = LocalDateTime.now().getDayOfYear();
-        final int lastUpdateDayOfYear = userWallet.getUpdateDate().getDayOfYear();
-        final int currentYear = LocalDateTime.now().getYear();
-        final int lastUpdateYear = userWallet.getUpdateDate().getYear();
-        final boolean currentDayIsSomeNextDayAfterLastUpdateDate = (currentYear > lastUpdateYear) && (currentDayOfYear > lastUpdateDayOfYear);
-
-        //daily update of previous wallet value
-        if (currentDayIsSomeNextDayAfterLastUpdateDate) {
-            final BigDecimal walletValue = userWallet.getWalletValue();
-            userWallet.setPreviousWalletValue(walletValue);
-        }
-
-        userWallet.setUpdateDate(LocalDateTime.now());
+        userWallet.setStockValue(stockValue.get());
+        userWallet.setLastUpdateDate(LocalDateTime.now(clock));
 
         final BigDecimal totalWalletValue = userWallet.getStockValue().add(userWallet.getBalanceAvailable());
         userWallet.setWalletValue(totalWalletValue);
 
         final BigDecimal differenceBetweenCurrentWalletValueAndPreviousWalletValue = userWallet.getWalletValue().subtract(userWallet.getPreviousWalletValue());
-        final BigDecimal hundredPercent = new BigDecimal(100);
+        final BigDecimal hundredPercent = new BigDecimal("100");
 
         userWallet.setWalletPercentageChange(differenceBetweenCurrentWalletValueAndPreviousWalletValue
-            .divide(userWallet.getPreviousWalletValue(), RoundingMode.HALF_DOWN)
-            .multiply(hundredPercent));
+            .divide(userWallet.getPreviousWalletValue(), 4, RoundingMode.HALF_UP)
+            .multiply(hundredPercent)
+            .setScale(2, RoundingMode.HALF_UP));
 
         return userWallet.toUserWalletDTO();
     }
--- src/test/java/pl/eizodev/app/user/service/UserWalletServiceTest.java
+++ src/test/java/pl/eizodev/app/user/service/UserWalletServiceTest.java
@@ -1,7 +1,119 @@
 package pl.eizodev.app.user.service;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import pl.eizodev.app.stock.dataprovider.StockDataProvider;
+import pl.eizodev.app.stock.dataprovider.dto.StockDataDTO;
+import pl.eizodev.app.stock.dataprovider.dto.StockQuoteDTO;
+import pl.eizodev.app.user.dto.UserRegisterDTO;
+import pl.eizodev.app.user.dto.UserWalletDTO;
+import pl.eizodev.app.user.exception.UserWalletNotFoundException;
+import pl.eizodev.app.user.model.User;
+import pl.eizodev.app.user.model.UserStock;
+import pl.eizodev.app.user.model.UserWallet;
+import pl.eizodev.app.user.repository.UserWalletRepository;
+
+import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class UserWalletServiceTest {
 
+    private static final String USERNAME = "user";
 
+    private static final BigDecimal USER_WALLET_BALANCE = new BigDecimal("10000.00");
+
+    private static final Clock CLOCK = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+
+    private UserWalletRepository userWalletRepositoryMock;
+
+    private StockDataProvider stockDataProviderMock;
+
+    private UserWalletService userWalletService;
+
+    private UserWallet userWallet;
+
+    @BeforeEach
+    void setUp() {
+        userWalletRepositoryMock = Mockito.mock(UserWalletRepository.class);
+        stockDataProviderMock = Mockito.mock(StockDataProvider.class);
+
+        userWalletService = new UserWalletService(userWalletRepositoryMock, stockDataProviderMock, CLOCK);
+
+        final UserRegisterDTO userRegisterDTO = new UserRegisterDTO(USERNAME, "test@test.pl", "password", USER_WALLET_BALANCE);
+        final User user = User.createUser(userRegisterDTO, "encryptedPassword");
+        userWallet = UserWallet.createNewUserWallet(user, USER_WALLET_BALANCE);
+    }
+
+    @Test
+    void shouldUpdateAndGetUserWalletWithNoStock() {
+        final BigDecimal previousWalletValue = new BigDecimal("15000.00");
+
+        userWallet.setPreviousWalletValue(previousWalletValue);
+        userWallet.setLastUpdateDate(LocalDateTime.of(2022, 4, 11, 17, 56, 22));
+        Mockito.when(userWalletRepositoryMock.findByUser_Username(USERNAME)).thenReturn(Optional.of(userWallet));
+
+        final UserWalletDTO result = userWalletService.updateAndGetUserWallet(USERNAME);
+
+        assertEquals(BigDecimal.ZERO, result.getStockValue());
+        assertEquals(USER_WALLET_BALANCE, result.getBalanceAvailable());
+        assertEquals(USER_WALLET_BALANCE, result.getWalletValue());
+        assertEquals(previousWalletValue, result.getPreviousWalletValue());
+        assertEquals(new BigDecimal("-33.33"), result.getWalletPercentageChange());
+        assertEquals(0, result.getUserStock().size());
+        assertEquals(LocalDateTime.now(CLOCK), result.getLastUpdateDate());
+    }
+
+    @Test
+    void shouldUpdateAndGetUserWalletWithStock() {
+        final BigDecimal previousWalletValue = new BigDecimal("17300.00");
+        final BigDecimal appleStockAveragePuchasePrice = new BigDecimal("25.00");
+        final BigDecimal amazonStockAveragePuchasePrice = new BigDecimal("32.00");
+        final List<UserStock> userStockList = List.of(
+            new UserStock(userWallet, "AAPL", appleStockAveragePuchasePrice, 100),
+            new UserStock(userWallet, "AMZN", amazonStockAveragePuchasePrice, 150)
+        );
+
+        final StockQuoteDTO appleStockQuote = new StockQuoteDTO("USD", new BigDecimal("30.00"), new BigDecimal("5.00"), new BigDecimal("20.00"),
+            new BigDecimal("32.00"), new BigDecimal("25.00"), new BigDecimal("26.00"), new BigDecimal("27.00"));
+        final StockDataDTO appleStock = new StockDataDTO("AAPL", "Apple Inc", "US", "NASDAQ NMS - GLOBAL MARKET", "Technology", "1980-12-12",
+            BigDecimal.valueOf(2530982), 16319.44, appleStockQuote, "2022-03-17T21:00:04");
+        final StockQuoteDTO amazonStockQuote = new StockQuoteDTO("USD", new BigDecimal("30.00"), new BigDecimal("-2.00"), new BigDecimal("7.00"),
+            new BigDecimal("32.00"), new BigDecimal("25.00"), new BigDecimal("26.00"), new BigDecimal("27.00"));
+        final StockDataDTO amazonStock = new StockDataDTO("AMZN", "Amazon Inc", "US", "NASDAQ NMS - GLOBAL MARKET", "E-Commerce", "1980-12-12",
+            BigDecimal.valueOf(2530982), 16319.44, amazonStockQuote, "2022-03-17T21:00:04");
+
+        userWallet.setUserStock(userStockList);
+        userWallet.setPreviousWalletValue(previousWalletValue);
+        userWallet.setLastUpdateDate(LocalDateTime.of(2022, 4, 11, 17, 56, 22));
+        Mockito.when(userWalletRepositoryMock.findByUser_Username(USERNAME)).thenReturn(Optional.of(userWallet));
+        Mockito.when(stockDataProviderMock.getStockData("AAPL")).thenReturn(appleStock);
+        Mockito.when(stockDataProviderMock.getStockData("AMZN")).thenReturn(amazonStock);
+
+        final UserWalletDTO result = userWalletService.updateAndGetUserWallet(USERNAME);
+
+        assertEquals(new BigDecimal("7500.00"), result.getStockValue());
+        assertEquals(USER_WALLET_BALANCE, result.getBalanceAvailable());
+        assertEquals(new BigDecimal("17500.00"), result.getWalletValue());
+        assertEquals(previousWalletValue, result.getPreviousWalletValue());
+        assertEquals(new BigDecimal("1.16"), result.getWalletPercentageChange());
+        assertEquals(2, result.getUserStock().size());
+        assertEquals(appleStockAveragePuchasePrice, result.getUserStock().get(0).getAveragePurchasePrice());
+        assertEquals(amazonStockAveragePuchasePrice, result.getUserStock().get(1).getAveragePurchasePrice());
+        assertEquals(LocalDateTime.now(CLOCK), result.getLastUpdateDate());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenUserWalletIsNotFound() {
+        Assertions.assertThrows(UserWalletNotFoundException.class, () -> userWalletService.updateAndGetUserWallet(USERNAME));
+    }
 
 }
