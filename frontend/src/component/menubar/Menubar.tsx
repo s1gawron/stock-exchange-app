89eb4fdb850d7888676d7c85160565bf22bc3848
s1gawron <gawron241@gmail.com>
2024-08-21 08:03:55 +0200

    React + Typescript frontend - menubar new button linking to stock listings

--- frontend/src/component/menubar/Menubar.tsx
+++ frontend/src/component/menubar/Menubar.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import {FaHome} from "react-icons/fa";
+import {FaChartLine, FaHome} from "react-icons/fa";
 import styles from "./styles.module.css";
 import LinkButton from "../linkButton/LinkButton";
 
 export default function Menubar(): React.ReactElement {
+
+    const isOnTargetPage = (target: string): boolean => {
+        const currentLocation: string = window.location.pathname;
+        return currentLocation.includes(target);
+    }
+
     return (
         <div id={styles.homeLink}>
             <div className={styles.menuBarLink}>
                 <LinkButton props={{linkTo: "/", icon: <FaHome size="35px"/>, text: "Home"}}/>
             </div>
+
+            {isOnTargetPage("stockListings") ? (<></>) :
+                (<div className={styles.menuBarLink}>
+                    <LinkButton props={{linkTo: "/stockListings/DJI", icon: <FaChartLine size="35px"/>, text: "Listings"}}/>
+                </div>)
+            }
+
+            <div style={{clear: "both"}}></div>
         </div>
     );
 }
