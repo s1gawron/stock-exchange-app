2d443a439d40ab3ff550ba2e634cac562ddca3c5
s1gawron <gawron241@gmail.com>
2024-08-21 13:33:39 +0200

    React + Typescript frontend - split view component

---
+++ frontend/src/component/splitView/SplitView.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+export default function SplitView({left, right}: { left: React.ReactNode, right: React.ReactNode }): React.ReactElement {
+    return (
+        <div id={styles.container}>
+            <div className={styles.square}>
+                {left}
+            </div>
+
+            <div className={styles.square}>
+                {right}
+            </div>
+
+            <div style={{clear: "both"}}></div>
+        </div>
+    );
+}
--- frontend/src/page/home/HomePage.tsx
+++ frontend/src/page/home/HomePage.tsx
@@ -7,6 +7,7 @@ import {TiInfoLarge} from "react-icons/ti";
 import {UserWalletDTO} from "../../dto/user/UserWalletDTO";
 import {getUserWalletDetails} from "../../util/UserWalletRestService";
 import Footer from "../../component/footer/Footer";
+import SplitView from "../../component/splitView/SplitView";
 
 const UNIX_TIME_ZERO: Date = new Date(1970, 0, 1);
 
@@ -27,68 +28,60 @@ export default function HomePage(): React.ReactElement {
 
     useEffect(() => getUserWalletDetails(setUserWallet), []);
 
-    return (
-        <div>
-            <Topbar/>
+    const left: React.ReactElement = <fieldset id={styles.actions}>
+        <legend>Actions:</legend>
 
-            <div id={styles.container}>
-                <div className={styles.square}>
-                    <fieldset id={styles.actions}>
-                        <legend>Actions:</legend>
-
-                        <div className={styles.actionBtnWrapper}>
-                            <Link to="/stockListings/DJI">
-                                <button id={styles.listingsBtn} className={styles.actionBtn}>
-                                    <div>Stock listings</div>
-                                    <div><FaChartLine className={styles.actionIcons}/></div>
-                                </button>
-                            </Link>
-                        </div>
-
-                        <div className={styles.actionBtnWrapper}>
-                            <Link to="/user/myWallet">
-                                <button id={styles.walletDetailsBtn} className={styles.actionBtn}>
-                                    <div>Wallet details</div>
-                                    <div><TiInfoLarge className={styles.actionIcons}/></div>
-                                </button>
-                            </Link>
-                        </div>
-                    </fieldset>
-                </div>
-
-                <div className={styles.square}>
-                    <fieldset id={styles.walletInfo}>
-                        <legend>My wallet:</legend>
-                        <div id={styles.walletLegend} className={styles.walletInfoFont}>
-                            <p>Wallet value:</p>
-                            <p>Balance available:</p>
-                            <p>Balance blocked:</p>
-                            <p>Stock value:</p>
-                            <br/>
-                            <p>Previous wallet value:</p>
-                            <p>Wallet percentage change:</p>
-                            <p>Update date:</p>
-                        </div>
-
-                        <div id={styles.walletData} className={styles.walletInfoFont}>
-                            <p>{numToFixed(userWallet.value)} USD</p>
-                            <p>{numToFixed(userWallet.balanceAvailable)} USD</p>
-                            <p> {numToFixed(userWallet.balanceBlocked)} USD</p>
-                            <p> {numToFixed(userWallet.stockValue)} USD</p>
-                            <br/>
-                            <p> {numToFixed(userWallet.lastDayValue)} USD</p>
-                            <p>{numToFixed(userWallet.valuePercentageChange)}%</p>
-                            <p>{new Date(userWallet.lastUpdateDate).toLocaleString()}</p>
-                        </div>
-
-                        <div style={{clear: "both"}}></div>
-                    </fieldset>
-                </div>
-
-                <div style={{clear: "both"}}></div>
-            </div>
-
-            <Footer text="Thank you for trading with us! Here's to your success in the market!"/>
+        <div className={styles.actionBtnWrapper}>
+            <Link to="/stockListings/DJI">
+                <button id={styles.listingsBtn} className={styles.actionBtn}>
+                    <div>Stock listings</div>
+                    <div><FaChartLine className={styles.actionIcons}/></div>
+                </button>
+            </Link>
         </div>
+
+        <div className={styles.actionBtnWrapper}>
+            <Link to="/user/myWallet">
+                <button id={styles.walletDetailsBtn} className={styles.actionBtn}>
+                    <div>Wallet details</div>
+                    <div><TiInfoLarge className={styles.actionIcons}/></div>
+                </button>
+            </Link>
+        </div>
+    </fieldset>;
+
+    const right: React.ReactElement = <fieldset id={styles.walletInfo}>
+        <legend>My wallet:</legend>
+        <div id={styles.walletLegend} className={styles.walletInfoFont}>
+            <p>Wallet value:</p>
+            <p>Balance available:</p>
+            <p>Balance blocked:</p>
+            <p>Stock value:</p>
+            <br/>
+            <p>Previous wallet value:</p>
+            <p>Wallet percentage change:</p>
+            <p>Update date:</p>
+        </div>
+
+        <div id={styles.walletData} className={styles.walletInfoFont}>
+            <p>{numToFixed(userWallet.value)} USD</p>
+            <p>{numToFixed(userWallet.balanceAvailable)} USD</p>
+            <p> {numToFixed(userWallet.balanceBlocked)} USD</p>
+            <p> {numToFixed(userWallet.stockValue)} USD</p>
+            <br/>
+            <p> {numToFixed(userWallet.lastDayValue)} USD</p>
+            <p>{numToFixed(userWallet.valuePercentageChange)}%</p>
+            <p>{new Date(userWallet.lastUpdateDate).toLocaleString()}</p>
+        </div>
+
+        <div style={{clear: "both"}}></div>
+    </fieldset>;
+
+    return (
+        <>
+            <Topbar/>
+            <SplitView left={left} right={right}/>
+            <Footer text="Thank you for trading with us! Here's to your success in the market!"/>
+        </>
     );
 }
