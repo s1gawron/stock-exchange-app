e97f3e55ca28097f7d3c6bfa3a0e30c91bca77bd
s1gawron <gawron241@gmail.com>
2025-02-01 20:46:08 +0100

    [BACKEND] Removing rabbit mechanism from transaction processing

--- backend/src/main/java/com/s1gawron/stockexchange/transaction/dao/TransactionDAO.java
+++ backend/src/main/java/com/s1gawron/stockexchange/transaction/dao/TransactionDAO.java
@@ -1,18 +1,12 @@
 package com.s1gawron.stockexchange.transaction.dao;
 
 import com.s1gawron.stockexchange.transaction.model.Transaction;
-import com.s1gawron.stockexchange.transaction.model.TransactionStatus;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface TransactionDAO {
 
     void saveTransaction(Transaction transaction);
-
-    List<Long> getNewTransactionIds();
-
-    void changeTransactionsStatus(List<Long> transactionIds, TransactionStatus newTransactionStatus);
 
     Optional<Transaction> getTransactionById(long transactionId);
 
--- backend/src/main/java/com/s1gawron/stockexchange/transaction/dao/impl/HibernateCriteriaTransactionDAO.java
+++ backend/src/main/java/com/s1gawron/stockexchange/transaction/dao/impl/HibernateCriteriaTransactionDAO.java
@@ -2,17 +2,14 @@ package com.s1gawron.stockexchange.transaction.dao.impl;
 
 import com.s1gawron.stockexchange.transaction.dao.TransactionDAO;
 import com.s1gawron.stockexchange.transaction.model.Transaction;
-import com.s1gawron.stockexchange.transaction.model.TransactionStatus;
 import com.s1gawron.stockexchange.transaction.model.Transaction_;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.CriteriaUpdate;
 import jakarta.persistence.criteria.Root;
 import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -24,33 +21,13 @@ public class HibernateCriteriaTransactionDAO implements TransactionDAO {
         this.entityManager = entityManager;
     }
 
-    @Override public void saveTransaction(final Transaction transaction) {
+    @Override
+    public void saveTransaction(final Transaction transaction) {
         getSession().persist(transaction);
     }
 
-    @Override public List<Long> getNewTransactionIds() {
-        final CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        final CriteriaQuery<Long> query = cb.createQuery(Long.class);
-        final Root<Transaction> root = query.from(Transaction.class);
-
-        query.select(root.get(Transaction_.transactionId))
-            .where(cb.equal(root.get(Transaction_.transactionStatus), TransactionStatus.NEW));
-
-        return getSession().createQuery(query).list();
-    }
-
-    @Override public void changeTransactionsStatus(final List<Long> transactionIds, final TransactionStatus newTransactionStatus) {
-        final CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        final CriteriaUpdate<Transaction> criteriaUpdate = cb.createCriteriaUpdate(Transaction.class);
-        final Root<Transaction> root = criteriaUpdate.from(Transaction.class);
-
-        criteriaUpdate.set(Transaction_.transactionStatus, newTransactionStatus)
-            .where(root.get(Transaction_.transactionId).in(transactionIds));
-
-        getSession().createMutationQuery(criteriaUpdate).executeUpdate();
-    }
-
-    @Override public Optional<Transaction> getTransactionById(final long transactionId) {
+    @Override
+    public Optional<Transaction> getTransactionById(final long transactionId) {
         final CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         final CriteriaQuery<Transaction> query = cb.createQuery(Transaction.class);
         final Root<Transaction> root = query.from(Transaction.class);
