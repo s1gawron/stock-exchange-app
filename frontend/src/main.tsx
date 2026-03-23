88f68dca6f3cda882cc23be089344a09d589d37d
s1gawron <gawron241@gmail.com>
2026-03-23 20:04:57 +0100

    [FRONTEND] named imports

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
-const root = ReactDOM.createRoot(
+const root = createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <React.StrictMode>
+    <StrictMode>
         <App/>
-    </React.StrictMode>
+    </StrictMode>
 );
 
